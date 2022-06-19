1bd9fe749ffed05d8d94baf7411ed5e6514bb83f
VKhodyrev <vkhodyrev1@gmail.com>
2022-06-19 20:41:18 +0300

    Added vpc example files

---
+++ cloudcasts.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  # Configuration options
+  profile = "cloudecasts"
+  region  = "us-east-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
